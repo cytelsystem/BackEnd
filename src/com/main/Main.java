9e90a74e05c3344dc064b2aaa2a41fbf441b7376
Hector Javier Moreno <cytelsystem@gmail.com>
2022-06-04 23:21:09 -0500

    funcionando ok

---
+++ src/com/main/Main.java
@@ -0,0 +1,51 @@
+package com.main;
+
+import com.main.Model.Odontologo;
+import com.main.persistence.dao.impl.OdontologoDAOH2;
+import com.main.Service.OdontologoService;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Main.class);
+
+        OdontologoService odontologoService = new OdontologoService();  // Instancio OdontologoService
+        odontologoService.setOdontologoIDAO(new OdontologoDAOH2()); // Utilizo un metodo de odontologoService y le paso una conexion OdontologoDAOH2()
+
+        //********************************Crear un nuevo objeto (Registro) Odontologo**************************************************************//
+
+
+//        Odontologo odontologo = new Odontologo(); //Instancio Odontologo
+//
+//        odontologo.setNombre("Graciela");
+//        odontologo.setApellido("Moreno Rodriguez");
+//        odontologo.setNumeroMatricula("41352450");
+//        odontologoService.guardaOdontologoService(odontologo); // le paso por parametro la informacion creada en el objeto odontologo
+
+        //****************Consultar Todos los registro de la base de datos tabla Odontologo*********************************************************//
+
+        // Debe existir un metodo toString en el Odontologo
+
+//            int ListarTodo = odontologoService.listarTodosServices().size();
+//
+//            for (int i=0; i< ListarTodo; i++) {
+//                Odontologo registros = odontologoService.listarTodosServices().get(i);
+//                logger.info(registros);
+//            }
+
+        //***************************************Consultar por un Solo ID tabla Odontologo***********************************************************//
+
+//        logger.info(odontologoService.buscarOdontologoService(24));
+
+        //****************************************Eliminar Registro por ID tabla Odontologo********************************************************//
+//         odontologoService.eliminar(18L);
+
+        //*****************************************************************************************************************************************//
+
+
+    }
+}
